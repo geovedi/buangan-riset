c7a633b5a291123882cae3b742f542d0882322b9
Jim Geovedi <jim@geovedi.com>
2015-07-07 14:20:19 +0700

    replace X-- and F--

---
+++ scripts/replace_unknown_words.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import codecs
+from nltk.tag.util import str2tuple, tuple2str
+
+
+def fix(corpus_ud, corpus_itb, corpus_out):
+    itb = map(lambda tagged: map(str2tuple, tagged.split()),
+              codecs.open(corpus_itb, 'r', 'utf-8').read().strip().split('\n'))
+    ud = map(lambda tagged: map(str2tuple, tagged.split()),
+             codecs.open(corpus_ud, 'r', 'utf-8').read().strip().split('\n'))
+
+    corpus = []
+    for x, y in zip(ud, itb):
+        sent = []
+        for xx, yy in zip(x, y):
+            xx, yy = list(xx), list(yy)
+            if xx[1] == 'PROPN':
+                yy[1] = 'E--'
+            if 'X--' in yy[1] or 'F--' in yy[1]:
+                if xx[1] == 'NOUN':
+                    yy[1] = yy[1].replace('X--', 'NSD').replace('F--', 'NSD')
+                elif xx[1] == 'VERB':
+                    yy[1] = yy[1].replace('X--', 'VSA').replace('F--', 'NSD')
+                elif xx[1] == 'ADJ':
+                    yy[1] = yy[1].replace('X--', 'ASP').replace('F--', 'ASP')
+                elif xx[1] == 'ADV':
+                    yy[1] = yy[1].replace('X--', 'D--').replace('F--', 'D--')
+                elif xx[1] == 'ADP':
+                    yy[1] = yy[1].replace('X--', 'R--').replace('F--', 'R--')
+                elif xx[1] == 'DET':
+                    yy[1] = yy[1].replace('X--', 'B--').replace('F--', 'B--')
+            sent.append(tuple2str(yy))
+        corpus.append(sent)
+
+    with codecs.open(corpus_out, 'w', 'utf-8') as out:
+        for sent in corpus:
+            out.write(' '.join(sent))
+            out.write('\n')
